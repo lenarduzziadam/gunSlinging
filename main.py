72616117b905f491c8e4ec101739f7595ff73cca
lenarduzziadam <lenarduzziadam@gmail.com>
2024-10-08 23:40:21 -0500

    gunslinger enemy made

--- main.py
+++ main.py
@@ -7,31 +7,111 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Gunslingers Vengence')
 
+#framerates
+clock = pygame.time.Clock()
+
+#moving booleans
+movingLeft = False
+movingRight = False
+
+#draws background
+def drawBG():
+    screen.fill(BLACK)
+
 #Gunslinger player class
 class Gunslinger(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
+    def __init__(self, charType, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('CowboyIMG/Cowboy4_idle with gun_0.png')
+        self.speed = speed
+        self.charType = charType
+        
+        self.direction = 1
+        self.flip = False
+        
+        img = pygame.image.load(f'{self.charType}.png')
 
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale) , (int(img.get_height() * scale))))
 
         self.rect = self.image.get_rect()
-        self.rect.center = (x, x)
+        self.rect.center = (x, y)
+        
+        
+    def move(self, movingLeft, movingRight):
+        #resets movment variables
+        dx = 0
+        dy = 0
+        #assign movments
+        if movingLeft:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+            
+        if movingRight:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+            
+        self.rect.x += dx
+        self.rect.y += dy
+        
+        
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+        
+    
 
-player = Gunslinger(p_startX, p_startY, PLAYER_SCALE)
-player2 = Gunslinger(400, 250, PLAYER_SCALE)
+
+#initilization of players
+player = Gunslinger('CowboyIMG/Cowboy4_idle with gun_0', p_startX, p_startY, PLAYER_SCALE, PLAYER_SPEED)
+enemy = Gunslinger('EnemyIMG/Cowboy2_idle with gun_0',400, 250, PLAYER_SCALE, PLAYER_SPEED)
+
 
 run = True
 while run:
     
-    screen.blit(player.image, player.rect)
-    screen.blit(player2.image, player2.rect)
+    clock.tick()
     
+    drawBG()
     
+    player.draw()
+    enemy.draw()
+    player.move(movingLeft, movingRight)
+
     for event in pygame.event.get():
         #quitting game
         if event.type == pygame.QUIT:
             run = False
+            
+        #keyboard presses
+        if event.type == pygame.KEYDOWN:
+        
+            if event.key == pygame.K_LEFT:
+                movingLeft = True        
+            if event.key == pygame.K_RIGHT:
+                movingRight = True
+            
+            if event.key == pygame.K_a:
+                movingLeft = True        
+            if event.key == pygame.K_d:
+                movingRight = True
+            
+            if event.key == pygame.K_ESCAPE:
+                run = False
+        
+        #keyboard release        
+        if event.type == pygame.KEYUP:
+        
+            if event.key == pygame.K_LEFT:
+                movingLeft = False
+            if event.key == pygame.K_RIGHT:
+                movingRight = False        
+                
+            if event.key == pygame.K_a:
+                movingLeft = False
+            if event.key == pygame.K_d:
+                movingRight = False        
+    
+    
     
     pygame.display.update()        
 
